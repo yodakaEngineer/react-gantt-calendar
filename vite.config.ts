66cb8bc77b94313f8719123626fae85b9be5c468
yodakaEngineer <engineeryodaka@gmail.com>
2022-07-21 12:55:08 +0900

    chore: use library mode on vite. build success.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import {ModuleFormat} from 'rollup'
+
+import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      name: "ReactTimeline",
+      fileName: (format: ModuleFormat) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
 })
