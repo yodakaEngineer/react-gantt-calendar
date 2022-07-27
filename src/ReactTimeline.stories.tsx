ad2ae461a95bee6f6b6828f116c4d3131977e225
yodakaEngineer <engineeryodaka@gmail.com>
2022-07-27 15:46:56 +0900

    feature: calc row span. and render table.

--- src/ReactTimeline.stories.tsx
+++ src/ReactTimeline.stories.tsx
@@ -7,7 +7,70 @@ export default {
   title: 'ReactTimeline',
 }
 
-const Template: ComponentStory<typeof ReactTimeline> = () => <ReactTimeline />
+const args = {
+  rowHeads: [
+    {
+      id: '1',
+      label: 'Hotel1',
+      childRowHeads: [
+        {
+          id: '1-1',
+          label: '1F',
+          childRowHeads: [
+            {
+              id: '1-1-1',
+              label: '101',
+            },
+            {
+              id: '1-1-2',
+              label: '102',
+            }
+          ]
+        },
+        {
+          id: '1-2',
+          label: '2F',
+          childRowHeads: [
+            {
+              id: '1-2-1',
+              label: '201',
+            },
+            {
+              id: '1-2-2',
+              label: '202',
+            }
+          ]
+        },
+      ]
+    },
+    {
+      id: '2',
+      label: 'Hotel2',
+    }
+  ],
+  rowContents: [
+    {
+      headIds: ['1', '1-1', '1-1-1'],
+      label: 'Cleaning1',
+      startAt: new Date('2020-01-01'),
+      endAt: new Date('2020-01-02'),
+    },
+    {
+      headIds: ['1', '1-1', '1-1-2'],
+      label: 'Cleaning2',
+      startAt: new Date('2020-01-01'),
+      endAt: new Date('2020-01-02'),
+    },
+    {
+      headIds: ['1', '1-2', '1-2-1'],
+      label: 'Cleaning3',
+      startAt: new Date('2020-01-01'),
+      endAt: new Date('2020-01-02'),
+    },
+  ]
+}
+
+const Template: ComponentStory<typeof ReactTimeline> = () => <ReactTimeline {...args} />
 
 export const Default = Template.bind({})
 Default.args = {}
--- src/ReactTimeline.tsx
+++ src/ReactTimeline.tsx
@@ -1,10 +1,85 @@
 // FIXME: I wanna allow user opt in.
 import './styles.scss'
+import React from 'react'
 
-export const ReactTimeline = () => {
+type RowHead = {
+  id: string | number
+  label: string | React.ReactNode
+  onClick?: (row: RowHead) => void
+  childRowHeads?: RowHead[]
+  rowSpan?: number
+}
+
+type RowContent = {
+  headIds: RowHead['id'][]
+  label: string | React.ReactNode
+  startAt: Date
+  endAt: Date
+}
+
+type TableRow = {
+  tableHeads: RowHead[]
+  tableData: RowContent
+}
+
+type Props = {
+  rowHeads: RowHead[]
+  rowContents: RowContent[]
+}
+
+export const ReactTimeline = (props: Props) => {
+  const { rowContents } = props
+  const recursiveRowSpans = (head: RowHead) => {
+    if (head.childRowHeads) {
+      head.childRowHeads = head.childRowHeads.map(recursiveRowSpans)
+    }
+    head.rowSpan = rowContents
+      .filter(content => content.headIds.includes(head.id))
+      .length
+    return head
+  }
+  const rowHeads = props.rowHeads.map(recursiveRowSpans)
+  const renderedHeadIds: RowHead['id'][] = []
+  const tableRows: TableRow[] = rowContents.map(content => {
+    const row: TableRow = {
+      tableHeads: [],
+      tableData: content,
+    }
+    rowHeads.forEach(head => {
+      if (content.headIds.includes(head.id)) {
+        if (!renderedHeadIds.includes(head.id)) {
+          row.tableHeads.push(head)
+          renderedHeadIds.push(head.id)
+        }
+        if (head.childRowHeads) {
+          head.childRowHeads.forEach(childHead => {
+            if (content.headIds.includes(childHead.id)) {
+              if (!renderedHeadIds.includes(childHead.id)) {
+                row.tableHeads.push(childHead)
+                renderedHeadIds.push(childHead.id)
+              }
+            }
+          })
+        }
+      }
+    })
+    return row
+  })
+
   return (
-    <div className={'reactTimeline'}>
-      hoge
-    </div>
+    <table className={'reactTimeline'}>
+      <thead>
+      </thead>
+      <tbody>
+      {
+        tableRows.map(row => (
+          <tr>
+            {row.tableHeads.map(head => (<th rowSpan={head.rowSpan}>{head.label}</th>))}
+            <td>{row.tableData.label}</td>
+          </tr>
+        ))
+      }
+      </tbody>
+    </table>
   )
 }
