7d1e9d0315730141c126c15130b5cd08bdb96ed3
yodakaEngineer <engineeryodaka@gmail.com>
2024-12-17 13:17:57 +0900

    fix: build type

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
-import { ModuleFormat } from 'rollup'
-
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
-    minify: false,
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
       name: 'ReactGanttCalendar',
-      fileName: (format: ModuleFormat) => `index.${format}.js`,
+      fileName: 'index',
+      formats: ['es', 'umd'],
       cssFileName: 'style',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
         },
       },
     },
